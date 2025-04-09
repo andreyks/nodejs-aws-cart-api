fcc9edda25321a821d9201d99bb6675457bae8a8
Andrii Kostromin <andrii_kostromin@epam.com>
2025-04-09 20:51:10 +0300

    Task 08: RDS - CDK deploy fixes

--- infrastructure/README.md
+++ infrastructure/README.md
@@ -1,11 +1,14 @@
-## About
-Using AWS CDK example https://github.com/aws-samples/aws-cdk-examples/tree/main/typescript/static-site.
+# Welcome to your CDK TypeScript project
 
-## Deploy
+This is a blank project for CDK development with TypeScript.
 
-```shell
-$ npm install -g aws-cdk
-$ npm install
-$ npm run build
-$ cdk deploy
-```
+The `cdk.json` file tells the CDK Toolkit how to execute your app.
+
+## Useful commands
+
+* `npm run build`   compile typescript to js
+* `npm run watch`   watch for changes and compile
+* `npm run test`    perform the jest unit tests
+* `npx cdk deploy`  deploy this stack to your default AWS account/region
+* `npx cdk diff`    compare deployed stack with current state
+* `npx cdk synth`   emits the synthesized CloudFormation template
--- infrastructure/Stack.ts
+++ infrastructure/Stack.ts
@@ -1,6 +1,10 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as rds from 'aws-cdk-lib/aws-rds';
+import * as iam from 'aws-cdk-lib/aws-iam';
+
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as path from 'path';
 import * as donenv  from 'dotenv';
@@ -11,11 +15,51 @@ export class NestjsLambdaStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
+        const DB_HOST = process.env.DB_HOST || '';
+        const DB_PORT = process.env.DB_PORT || '';
+        const DB_NAME = process.env.DB_NAME || '';
+        const DB_USERNAME = process.env.DB_USERNAME || '';
+        const DB_PASSWORD = process.env.DB_PASSWORD || '';
+        const VPC_ID = process.env.VPC_ID || '';
+        const RDS_SECURITY_GROUP_ID = process.env.RDS_SECURITY_GROUP_ID || '';
+
+        if (
+        !(
+            DB_HOST &&
+            DB_PORT &&
+            DB_NAME &&
+            DB_USERNAME &&
+            DB_PASSWORD &&
+            VPC_ID &&
+            RDS_SECURITY_GROUP_ID
+        )
+        ) {
+            console.log("Error: Wrong environment variables!")
+        }
+
+        // Assuming you have an existing VPC
+        // const vpc = ec2.Vpc.fromLookup(this, 'VPC', { isDefault: true });
+        const vpc = ec2.Vpc.fromLookup(this, 'VPC', { vpcId: VPC_ID });
+
+        // Create a security group for the Lambda function
+        const lambdaSG = new ec2.SecurityGroup(this, 'LambdaSecurityGroup', {
+            vpc,
+            allowAllOutbound: true,
+            description: 'Security group for Lambda function',
+        });
+    
+        // A security group for the RDS instance
+        const dbSG = ec2.SecurityGroup.fromSecurityGroupId(this, 'DatabaseSecurityGroup', RDS_SECURITY_GROUP_ID);
+
+        // Allow the Lambda security group to access the RDS security group
+        dbSG.addIngressRule(lambdaSG, ec2.Port.tcp(5432), 'Allow Lambda access');
+
         const server = new NodejsFunction(this, 'nodejs-aws-cart-api', {
             functionName: 'nodejs-aws-cart-api',
-            runtime: lambda.Runtime.NODEJS_20_X,
+            runtime: lambda.Runtime.NODEJS_22_X,
             entry: path.join(__dirname, '../src/main.lambda.ts'),
             handler: 'handler',
+            // depsLockFilePath: path.join(__dirname, '../../package-lock.json'),
             timeout: cdk.Duration.seconds(30),
             memorySize: 128,
             environment: {
@@ -45,7 +89,18 @@ export class NestjsLambdaStack extends cdk.Stack {
                     'reflect-metadata',
                 ],
             },
+            allowPublicSubnet: true,
+            vpc: vpc,
+            vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
+            securityGroups: [lambdaSG],
         });
+
+        // Grant the Lambda function permissions to access RDS
+        server.addToRolePolicy(new iam.PolicyStatement({
+            actions: ['rds-db:connect'],
+            // resources: ['arn:aws:rds-db:eu-north-1:account-id:dbuser:db-resource-id/database-user'],
+            resources: ['arn:aws:rds:eu-north-1:140023362902:db:shop'],
+        }));
 
         const { url } = server.addFunctionUrl({
             authType: lambda.FunctionUrlAuthType.NONE,
@@ -61,5 +116,61 @@ export class NestjsLambdaStack extends cdk.Stack {
         })
 
         new cdk.CfnOutput(this, 'FunctionUrl', { value: url });
+
+
+        // // Create VPC
+        // const vpc = new ec2.Vpc(this, 'LambdaRDS', {
+        //     maxAzs: 2,
+        //     natGateways: 0,
+        // });
+    
+        // // Create RDS instance
+        // const dbInstance = new rds.DatabaseInstance(this, 'MyRDSInstance', {
+        //     engine: rds.DatabaseInstanceEngine.postgres({ version: rds.PostgresEngineVersion.VER_17 }),
+        //     instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
+        //     vpc,
+        //     vpcSubnets: { subnetType: ec2.SubnetType.PRIVATE_ISOLATED },
+        //     databaseName: process.env.DB_NAME || 'shop',
+        //     allocatedStorage: 20,
+        //     maxAllocatedStorage: 20,
+        //     deleteAutomatedBackups: true,
+        //     removalPolicy: cdk.RemovalPolicy.DESTROY,
+        //     deletionProtection: false,
+        //     multiAz: false,
+        //     publiclyAccessible: false,
+        //     credentials: rds.Credentials.fromPassword(process.env.DB_USERNAME, cdk.SecretValue.plainText('postgres'))
+        // });
+    
+        // // Create EC2 instance
+        // const ec2Instance = new ec2.Instance(this, 'MyEC2Instance', {
+        //     vpc,
+        //     vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
+        //     instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
+        //     machineImage: ec2.MachineImage.latestAmazonLinux2023(),
+        // });
+    
+        // // Allow EC2 instance to connect to RDS
+        // dbInstance.connections.allowFrom(ec2Instance, ec2.Port.tcp(5432));
+    
+        // // Install psql client on EC2 instance
+        // ec2Instance.addUserData(
+        //     'sudo dnf install postgresql15'
+        // );
+    
+        // // Create Lambda function
+        // const lambdaFunction = new lambda.Function(this, 'MyLambdaFunction', {
+        //     runtime: lambda.Runtime.NODEJS_22_X,
+        //     handler: 'index.handler',
+        //     code: lambda.Code.fromAsset('lambda'),
+        //     vpc,
+        //     vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
+        // });
+    
+        // // Allow Lambda to connect to RDS
+        // dbInstance.grantConnect(lambdaFunction);
+    
+        // // Output connection information
+        // new cdk.CfnOutput(this, 'RDS Endpoint', { value: dbInstance.dbInstanceEndpointAddress });
+        // new cdk.CfnOutput(this, 'EC2 Public IP', { value: ec2Instance.instancePublicIp });
     }
 }
